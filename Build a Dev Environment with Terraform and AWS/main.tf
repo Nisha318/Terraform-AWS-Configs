4a9d5c325737fed320532052c8aaa10bdfed251c
Nisha Prudhomme <eunishap@gmail.com>
2023-01-26 01:31:12 -0600

    Add files via upload

---
+++ Build a Dev Environment with Terraform and AWS/main.tf
@@ -0,0 +1,116 @@
+resource "aws_vpc" "nisha_vpc" {
+
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev"
+
+  }
+}
+
+resource "aws_subnet" "Public_Subnet_1" {
+  vpc_id                  = aws_vpc.nisha_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "nisha_internet_gateway" {
+  vpc_id = aws_vpc.nisha_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "nisha_public_rt" {
+
+  vpc_id = aws_vpc.nisha_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+
+  }
+
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.nisha_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.nisha_internet_gateway.id
+
+}
+
+resource "aws_route_table_association" "nisha_public_assoc" {
+
+  subnet_id      = aws_subnet.Public_Subnet_1.id
+  route_table_id = aws_route_table.nisha_public_rt.id
+
+}
+
+resource "aws_security_group" "nisha_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.nisha_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["108.196.141.160/32"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
+
+resource "aws_key_pair" "nisha_auth" {
+
+  key_name   = "nishakey"
+  public_key = file("~/.ssh/nishakey.pub")
+
+}
+
+resource "aws_instance" "dev_ec2" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.nisha_auth.id
+  vpc_security_group_ids = [aws_security_group.nisha_sg.id]
+  subnet_id = aws_subnet.Public_Subnet_1.id
+  user_data = file("userdata.tpl")
+
+ 
+   root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev_ec2"
+  }
+
+  provisioner "local-exec" {
+
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
+      hostname = self.public_ip
+      user = "ubuntu"
+      identityfile = "~/.ssh/nishakey"
+    })
+    interpreter = var.host_os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
+  }
+
+}
+
+
---
+++ Build a Dev Environment with Terraform and AWS/outputs.tf
@@ -0,0 +1,9 @@
+output "dev_ip" {
+
+    value = aws_instance.dev_ec2.public_ip 
+}
+
+
+
+
+
